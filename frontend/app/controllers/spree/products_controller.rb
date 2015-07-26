4580e808028a4635891657949f18df68edc5ee15
Frank <frankiek3@users.noreply.github.com>
2015-07-26 10:11:03 -0400

    Remove unused variable
    Hasn't been use since 2011, any given extensions could be using it though

--- frontend/app/controllers/spree/products_controller.rb
+++ frontend/app/controllers/spree/products_controller.rb
@@ -15,7 +15,6 @@ module Spree
     end
 
     def show
-      @variants = @product.variants_including_master.active(current_currency).includes([:option_values, :images])
       @product_properties = @product.product_properties.includes(:property)
       @taxon = Spree::Taxon.find(params[:taxon_id]) if params[:taxon_id]
     end
